460f84980d361519db755c1ac76f392d31e37558
Azmil Awaludin <azmilawaludin@gmail.com>
2022-11-18 16:59:06 +0700

    fix word

--- src/Todolist.js
+++ src/Todolist.js
@@ -4,7 +4,7 @@ function Todolist() {
   return (
     <div className={`w-screen min-h-screen bg-slate-100`}>
       <h2 className=" pt-[41px] font-bold text-3xl text-center">
-        Mengodonf Todo List
+        Mengodong Todo List
       </h2>
       <div
         className={`lg:mx-[30px] flex space-x-[35px] mt-[36px] sm:mx-[10px]`}
